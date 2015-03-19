20eba480dcda90890fa46761cfb7b59ae7539b67
Raja D <rajadura@cisco.com>
2015-03-19 21:17:26 +0530

    Airfinder init commit

---
+++ AirFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirFinder
+//
+//  Created by Raja Duraisamy on 19/03/15.
+//  Copyright (c) 2015 Raja Duraisamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
