5058036fecb8dc910a6fedfe103e0d4a40a24fe8
Raja D <rajadura@cisco.com>
2015-03-20 23:14:44 +0530

    init design

--- AirFinder/ViewController.swift
+++ AirFinder/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var menuView:UIView!
+    @IBOutlet var searchView:UIView!
+    @IBOutlet var listView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        // Set menu view border
+//        menuView.layer.cornerRadius = 30.0
+//        menuView.layer.borderColor = UIColor.redColor().CGColor
+//        menuView.layer.borderWidth = 1.0
+//        menuView.clipsToBounds = true
+//        menuView.layer.shadowColor = UIColor.blackColor().CGColor
+//        menuView.layer.shadowOpacity = 0.8
+//        menuView.layer.shadowRadius = 3.0
+//        menuView.layer.shadowOffset = CGSizeMake(2.0, 2.0);
+//
+//        // Set menu view border
+//        searchView.layer.cornerRadius = 30.0
+//        searchView.layer.borderColor = UIColor.redColor().CGColor
+//        searchView.layer.borderWidth = 1.0
+//        searchView.clipsToBounds = true
+//        searchView.layer.shadowColor = UIColor.blackColor().CGColor
+//        searchView.layer.shadowOpacity = 0.8
+//        searchView.layer.shadowRadius = 3.0
+//        searchView.layer.shadowOffset = CGSizeMake(2.0, 2.0);
+//        
+//        // Set menu view border
+//        listView.layer.cornerRadius = 30.0
+//        listView.layer.borderColor = UIColor.redColor().CGColor
+//        listView.layer.borderWidth = 1.0
+//        listView.clipsToBounds = true
+//        listView.layer.shadowColor = UIColor.blackColor().CGColor
+//        listView.layer.shadowOpacity = 0.8
+//        listView.layer.shadowRadius = 3.0
+//        listView.layer.shadowOffset = CGSizeMake(2.0, 2.0);
+
+        
     }
 
     override func didReceiveMemoryWarning() {
